12c5d5fb17eaecbb347550566ccccb0e6a82c222
JoelJohn08 <jmj6182@psu.edu>
2020-08-26 09:54:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+name = input("Enter your name:")
+
+print("hello," + name +"!")
+age = input("Enter your age: ")
+age = int(age)
+print(age +1)
+
+print("you will be" str(age+1)+ "next year")
+print(f"I will be {age+1} next year")
