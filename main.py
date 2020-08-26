7fb10104119e1a3190a89921a1ce05a2f729075f
JoelJohn08 <jmj6182@psu.edu>
2020-08-26 12:23:45 +0000

    everything

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print(" Hello world")
 name = input("Enter your name:")
 
-print("hello, " + name +"!")
+print(" Hello, " + name +"!")
 age = input("Enter your age: ")
 age = int(age)
 print(f"You will be {age+1} next year")
