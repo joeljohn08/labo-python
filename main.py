83580b2b099b3b196e95a16e05db792318ffd3c5
JoelJohn08 <jmj6182@psu.edu>
2020-08-26 12:22:34 +0000

    Caps

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-print(" hello world")
+print(" Hello world")
 name = input("Enter your name:")
 
-print("hello," + name +"!")
+print("hello, " + name +"!")
 age = input("Enter your age: ")
 age = int(age)
-print(f"you will be {age+1} next year")
+print(f"You will be {age+1} next year")
